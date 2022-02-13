4158ad269c181ef27a1c4094cfa1a8f0ab3a8f7d
nagoy <nagoymail@gmail.com>
2022-02-13 19:50:28 +0500

    задание в разработке

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 window.addEventListener('DOMContentLoaded', function () {
 
     'use strict';
+    // Переключатель табов на странице
+    // Начало скрипта
     let tab = document.querySelectorAll('.info-header-tab'),
         info = document.querySelector('.info-header'),
         tabContent = document.querySelectorAll('.info-tabcontent');
@@ -20,6 +22,7 @@ window.addEventListener('DOMContentLoaded', function () {
             tabContent[b].classList.add('show');
         }
     }
+    showTabContent(0);
 
     info.addEventListener('click', function (event) {
         let target = event.target;
@@ -33,4 +36,46 @@ window.addEventListener('DOMContentLoaded', function () {
             }
         }
     });
+    // Конец скрипта переключателя
+
+
+    // Начало скрипта оформления таймера
+    let deadLine = '2022-02-22';
+
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+            seconds = Math.floor((t / 1000) % 60),
+            minutes = Math.floor((t / 1000 / 60) % 60),
+            hours = Math.floor((t / (1000 * 60 * 60)));
+        // hoursDays = Math.floor((t / 1000 / 60 / 60) % 24);
+        // days = Math.floor((t / (1000*60*60*24)));
+
+        return {
+            'total': t,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+            hours.textContent = t.hours;
+            minutes.textContent = t.minutes;
+            seconds.textContent = t.seconds;
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+    }
+
+    setClock('timer', deadLine);
 });
